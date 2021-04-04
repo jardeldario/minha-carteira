1313e61ed16f30aa8ad73b40b08bd404a52cc5d1
Jardel Dário <jardel.dario@gmail.com>
2021-04-04 00:02:26 -0300

    finish lesson: Aula 20 (finish Seçao 3)

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,29 +4,29 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
 
     const months = [
-        {value: 'Janeiro', label: 'Janeiro'},
-        {value: 'Fevereiro', label: 'Fevereiro'},
-        {value: 'Março', label: 'Março'},
-        {value: 'Abril', label: 'Abril'},
-        {value: 'Maio', label: 'Maio'},
-        {value: 'Junho', label: 'Junho'},
-        {value: 'Julho', label: 'Julho'},
-        {value: 'Agosto', label: 'Agosto'},
-        {value: 'Setembro', label: 'Setembro'},
-        {value: 'Outubro', label: 'Outubro'},
-        {value: 'Novembro', label: 'Novembro'},
-        {value: 'Dezembro', label: 'Dezembro'}
+        {value: '1', label: 'Janeiro'},
+        {value: '2', label: 'Fevereiro'},
+        {value: '3', label: 'Março'},
+        {value: '4', label: 'Abril'},
+        {value: '5', label: 'Maio'},
+        {value: '6', label: 'Junho'},
+        {value: '7', label: 'Julho'},
+        {value: '8', label: 'Agosto'},
+        {value: '9', label: 'Setembro'},
+        {value: '10', label: 'Outubro'},
+        {value: '11', label: 'Novembro'},
+        {value: '12', label: 'Dezembro'}
     ]
 
     const years = [
-        {value: '2019', label: '2019'},
+        {value: '2021', label: '2021'},
         {value: '2020', label: '2020'},
-        {value: '2021', label: '2021'}
+        {value: '2019', label: '2019'}
     ]
 
     return (
@@ -35,6 +35,15 @@ const List: React.FC = () => {
                 <SelectInput options={months}/>
                 <SelectInput options={years}/>
             </ContentHeader>
+
+            <Filters>
+                <button type="button" className="tag-filter tag-filter-recurrent">
+                    Recorrentes
+                </button>
+                <button type="button" className="tag-filter tag-filter-eventual">
+                    Eventuais
+                </button>
+            </Filters>
 
             <Content>
                 <HistoryFinanceCard
